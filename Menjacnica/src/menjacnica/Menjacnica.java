8d3c199e6d0df7e81d3d7a725e247c9f1a22e19e
Jovana Mijalkovic <jovana.mijalkovic96@gmail.com>
2017-04-02 20:18:26 +0200

    Implementirana je metoda dodavanjeKursa.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,14 +1,29 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs{
+	LinkedList<Valuta> valute=new LinkedList<Valuta>();
 	@Override
 	public void dodavanjeKursa(String naziv, double prodajni, double srednji, double kupovni, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		
+		if(valute.isEmpty())
+			throw new RuntimeException("Lista valuti je prazna!");
+		else{
+		 for(int i=0;i<valute.size();i++){
+		 	if(valute.get(i).getNaziv().equals(naziv)){
+		 		Kurs k=new Kurs();
+				k.setDatum(datum);
+		 		k.setKupovni(kupovni);
+		 		k.setProdajni(prodajni);
+				k.setSrednji(srednji);
+		 		valute.get(i).getKursevi().add(k);
+		 		break;
+		 	}
+		 }
+		}
 	}
 	@Override
 	public void brisanjeKursa(String naziv, GregorianCalendar datum) {
