ed3977ca7767f43f0e58070651697cb72edce0ca
Jovana Mijalkovic <jovana.mijalkovic96@gmail.com>
2017-04-02 19:28:45 +0200

    Dodata je nova klasa Menjacnica koja implementira interfejs MenjacnicaInterfejs.

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,23 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs{
+	@Override
+	public void dodavanjeKursa(String naziv, double prodajni, double srednji, double kupovni, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void brisanjeKursa(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public Kurs pronalazenjeKursa(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
